25191e81f1da6696a7d1f94b8f3d5457d977654c
asadullah00alimran <asadullahalimran00@gmail.com>
2021-11-25 12:05:53 +0600

    Changing CSS with Js JS file

---
+++ css_select.js
@@ -0,0 +1,9 @@
+function addStyle(){
+    var x = document.querySelector("#paraId");
+    x.classList.add("para-style");
+}
+
+function removeStyle(){
+    var x = document.querySelector("#paraId");
+    x.classList.remove("para-style");
+}
